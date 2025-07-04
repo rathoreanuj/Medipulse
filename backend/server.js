ec9bd005040fab17c679fcaef64a814405934598
rathoreanuj <anujrathore385@gmail.com>
2025-07-04 17:14:17 +0530

    file upload

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
+import userRouter from "./routes/userRoute.js"
+import doctorRouter from "./routes/doctorRoute.js"
+import adminRouter from "./routes/adminRoute.js"
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/user", userRouter)
+app.use("/api/admin", adminRouter)
+app.use("/api/doctor", doctorRouter)
+
+app.get("/", (req, res) => {
+  res.send("API Working")
+});
+
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
